7208091f8cf2f64b9031151532ba4c904944d634
Armando Guarino <armandoguarino@Armandos-MBP.local>
2021-06-25 17:59:31 +0200

    add channels page styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Noto Sans', 'sans-serif']
+      },
+      colors: {
+        'dark-blue': '#120F13',
+        'dark-gray': '#252329',
+        'md-gray': '#3C393F',
+        'light-gray': '#828282',
+        'extralight-gray': '#E0E0E0',
+        'accent-blue': '#2F80ED',
+        'danger-red': '#EB5757'
+      },
+    },
   },
   variants: {
     extend: {},
